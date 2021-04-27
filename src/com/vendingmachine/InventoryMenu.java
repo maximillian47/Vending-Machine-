fd4af229311a0770db681a6de0b7e799c320ef19
Max <xiao.ma_1@outlook.com>
2021-04-26 20:33:53 -0700

    doc: menu added

---
+++ src/com/vendingmachine/InventoryMenu.java
@@ -0,0 +1,8 @@
+package com.vendingmachine;
+
+public class InventoryMenu {
+    public static void display() {
+        System.out.println("Check out our drinks and snacks!");
+        for(Inventory)
+    }
+}
--- src/com/vendingmachine/VendingMachine.java
+++ src/com/vendingmachine/VendingMachine.java
@@ -1,4 +1,16 @@
 package com.vendingmachine;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class VendingMachine {
+    private static void Menu() {
+
+    }
+
+    private static void displayInventory() {
+        System.out.println("Look at what we have here!");
+
+
+    }
 }
